223ef7ad8c471a8edb40d4f7b5b032df8eec37ad
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2025-01-30 12:27:24 +0200

    initial commit

---
+++ dist/index.cf986275.js
@@ -0,0 +1,2 @@
+document.getElementById("color");const e=document.createElement("button");e.textContent="Click Me",document.body.appendChild(e),e.addEventListener("click",()=>{e.style.backgroundColor="red"});
+//# sourceMappingURL=index.cf986275.js.map
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const colorBtn = document.getElementById("color");
+const btn = document.createElement("button");
+btn.textContent = "Click Me"; // Add text to the button
+
+
+  document.body.appendChild(btn);
+
+  btn.addEventListener("click", () => {
+    btn.style.backgroundColor = "red";
+  });
+
