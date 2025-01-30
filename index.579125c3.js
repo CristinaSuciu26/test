1e432d99416ca753be0cb67b023dcd9e318b4208
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2025-01-30 12:22:21 +0200

    Updates

---
+++ index.579125c3.js
@@ -0,0 +1,9 @@
+const colorBtn = document.getElementById("color");
+const btn = document.createElement("button");
+btn.textContent = "Click Me"; // Add text to the button
+document.body.appendChild(btn);
+btn.addEventListener("click", ()=>{
+    btn.style.backgroundColor = "red";
+});
+
+//# sourceMappingURL=index.579125c3.js.map
